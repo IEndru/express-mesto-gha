b2c2dc0479c13ffaf8f182fa3f98657cd95600d3
Andrey Zverev <a.zverev@severnaya.ru>
2023-11-11 19:29:47 +0300

    did project 13 correction 4

--- controllers/cards.js
+++ controllers/cards.js
@@ -98,7 +98,7 @@ const likeCard = async (req, res) => {
     }
     res.send(card);
   } catch (err) {
-    if (err.name === 'ValidationError') {
+    if (err.name === 'CastError') {
       res.status(400).send({
         message: 'Переданы некорректные данные',
       });
