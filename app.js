1de6c454677b3f35e56999c4590829f464fb2098
Andrey Zverev <a.zverev@severnaya.ru>
2023-11-11 18:46:49 +0300

    did project 13 correction 3

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   })
   .catch((error) => {
     console.log('Ошибка при подключении к MongoDB:', error);
-    console.error(error);
   });
 
 app.use(helmet());
